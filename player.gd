a53265ae916c5c3bea355a089740305a33c920e2
Impendable <105776541+Impendable@users.noreply.github.com>
2026-08-14 11:14:17 -0400

    Initial commit: Moving Square, Clamped In Bounds

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed := 300.0
+
+var min_x: float
+var max_x: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var half_width := 20.0
+	min_x = half_width
+	max_x = get_viewport_rect().size.x - half_width
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0.0:
+		print("AXIS: ", direction)
+	position.x += direction * speed * delta
+	position.x = clamp(position.x, min_x, max_x)
+	
+func _input(event: InputEvent) -> void:
+	print("EVENT: ", event)
